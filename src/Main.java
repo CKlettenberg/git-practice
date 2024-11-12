65efcd7e03bc1ee45a457f5a3a0b306deb4c1d22
opilane <klettenberg@gmail.com>
2024-11-12 11:34:44 +0200

    Merge branch 'feature/test2'
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("TERETERETERE!");
-        String text = "LALALALALALALALALALALALALALALALALAL";
+        System.out.println("Hello people!");
+        String text = "New world";
         System.out.println(text);
 
-        int number = 999999999;
+        int number = 10;
         System.out.println(number);
     }
 }
